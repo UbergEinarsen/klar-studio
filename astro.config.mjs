6445af8d7397ab190e13a704dca0ce504d2a95b2
NicolaiU07 <ubergnicolai@gmail.com>
2026-06-22 01:57:52 +0200

    fix: remove unauthorized @types/node dep and simplify sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://klarstudio.no",
-  integrations: [react(), sitemap({ changefreq: "weekly", priority: 0.7 })],
+  integrations: [react(), sitemap()],
 });
